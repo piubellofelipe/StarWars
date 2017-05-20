5797f5cea403f1562f3e89d898fcebdbd065eb62
Felipe Piubello <Felipe Piubello>
2017-05-20 20:49:43 -0300

    added an alert for the user on the first (and only for the first) visit to the webapp

---
+++ public/cookie.js
@@ -0,0 +1,30 @@
+window.onload = (function () {
+    var visit = getCookie("cookie-visitor");
+    if (visit == null) {
+        alert("Bem vindo! Esta é a sua primeira visita!");
+        var expire = new Date();
+        expire = new Date(expire.getTime() + 7776000000);
+        document.cookie = "cookie=here; expires=" + expire;
+    }
+
+});
+
+
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 import css from './style/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
