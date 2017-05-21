2ea6d812cca8a06928d87e74608b1e1fad02477d
Felipe Piubello <Felipe Piubello>
2017-05-20 22:06:59 -0300

    still fixing https over http request

--- src/components/selected_character.js
+++ src/components/selected_character.js
@@ -10,7 +10,9 @@ const selectedCharacter = (props) => {
     var moviesList = movies.map( movie => {
         var response = "";
         var req = new XMLHttpRequest();
-        req.open('GET', movie.replace("http", "https"), false);
+        var url = movie.replace("http", "https");
+        console.log(url);
+        req.open('GET', url, false);
         req.addEventListener('load', function(){
             if (req.status >= 200 && req.status < 400){
                 response = JSON.parse(req.responseText);
